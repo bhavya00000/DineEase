9eda6a523464829ded11041d314d8a985af41a91
bhavya00000 <mehtabhavya1805@gmail.com>
2024-04-07 20:28:29 +0530

    backend commit

---
+++ backend/database/dbconnection.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+export const dbconnection = () =>{
+    mongoose.connect(process.env.MONGO_URL,{
+        dbName: "Restaurant"
+    }).then(()=>{
+        console.log("Connected to database success.");
+    }).catch(()=>{
+        console.log(`Some error occurred while connecting the database! ${err}`);
+    })
+}
+
